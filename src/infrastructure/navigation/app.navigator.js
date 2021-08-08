0a41b476b1509b3d94049ae8b8da2867c9caab33
Willian Negishi <willian_jvm@hotmail.com>
2021-08-09 02:26:44 +0900

    setting up the login,register screen and authentication

--- src/infrastructure/navigation/app.navigator.js
+++ src/infrastructure/navigation/app.navigator.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { Ionicons } from "@expo/vector-icons";
 import { Text } from "react-native";
@@ -39,23 +38,21 @@ const createScreenOptions = ({ route }) => {
 };
 
 export const AppNavigator = () => (
-  <NavigationContainer>
-    <Tab.Navigator screenOptions={createScreenOptions}>
-      <Tab.Screen
-        options={{ headerShown: false }}
-        name="Restaurants"
-        component={RestaurantsNavigator}
-      />
-      <Tab.Screen
-        options={{ headerShown: false }}
-        name="Map"
-        component={MapScreen}
-      />
-      <Tab.Screen
-        options={{ headerShown: false }}
-        name="Settings"
-        component={Settings}
-      />
-    </Tab.Navigator>
-  </NavigationContainer>
+  <Tab.Navigator screenOptions={createScreenOptions}>
+    <Tab.Screen
+      options={{ headerShown: false }}
+      name="Restaurants"
+      component={RestaurantsNavigator}
+    />
+    <Tab.Screen
+      options={{ headerShown: false }}
+      name="Map"
+      component={MapScreen}
+    />
+    <Tab.Screen
+      options={{ headerShown: false }}
+      name="Settings"
+      component={Settings}
+    />
+  </Tab.Navigator>
 );
